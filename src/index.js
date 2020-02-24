d951b3b6a84311054834e282e6b2e188f131552d
Kopochev <copochev@yandex.ru>
2020-02-24 18:19:33 +0300

    feat: task is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let absN = Math.abs(n);
+    let rev = 0;
+    
+    while (absN > 0) {
+        rev = rev * 10 + absN % 10;
+        absN = Math.floor(absN / 10);
+    }
+    return rev;
 }
